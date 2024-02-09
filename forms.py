a438f2d4455d979d9dd0c086c0e2adfa9543c73f
JESSICA <95001228+desarrolloJess@users.noreply.github.com>
2024-02-09 17:53:57 -0600

    IDGS803 09/02/24 Resistencias

--- forms.py
+++ forms.py
@@ -6,3 +6,68 @@ class distanciaPuntos(Form):
     X2=IntegerField('X2')
     Y1=IntegerField('Y1')
     Y2=IntegerField('Y2')
+
+class horoscopoChino(Form):
+    ANIO=StringField('ANIO')
+
+class signoZodiacal(Form):
+    dia=StringField('dia')
+    mes = SelectField('Mes',
+    choices=[
+        ('Enero', 'Enero'),
+        ('Febrero', 'Febrero'),
+        ('Marzo', 'Marzo'),
+        ('Abril', 'Abril'),
+        ('Mayo', 'Mayo'),
+        ('Junio', 'Junio'),
+        ('Julio', 'Julio'),
+        ('Agosto', 'Agosto'),
+        ('Septiembre', 'Septiembre'),
+        ('Octubre', 'Octubre'),
+        ('Noviembre', 'Noviembre'),
+        ('Diciembre', 'Diciembre')
+    ])
+
+
+class calculaResistencias(Form):
+    color1=SelectField('color1',
+    choices=[
+        ('0','Negro'),
+        ('1','Cafe'),
+        ('2','Rojo'),
+        ('3','Naranja'),
+        ('4','Amarillo'),
+        ('5','Verde'),
+        ('6','Azul'),
+        ('7','Violeta'),
+        ('8','Gris'),
+        ('9','Blanco')
+    ])  
+    
+    color2=SelectField('color2',
+    choices=[
+        ('0','Negro'),
+        ('1','Cafe'),
+        ('2','Rojo'),
+        ('3','Naranja'),
+        ('4','Amarillo'),
+        ('5','Verde'),
+        ('6','Azul'),
+        ('7','Violeta'),
+        ('8','Gris'),
+        ('9','Blanco')
+    ])
+    color3=SelectField('color3',
+    choices=[
+        ('1','Negro'),
+        ('10','Cafe'),
+        ('100','Rojo'),
+        ('1000','Naranja'),
+        ('10000','Amarillo'),
+        ('100000','Verde'),
+        ('1000000','Azul'),
+        ('10000000','Violeta'),
+        ('100000000','Gris'),
+        ('1000000000','Blanco')
+    ])    
+    
--- main.py
+++ main.py
@@ -28,6 +28,75 @@ def distancia():
 
     return render_template("distancia.html",form=distancia_form,resultado=resultado)
 
+@app.route("/resistencias",methods=["GET","POST"])
+def resistencias():
+    resultado = ""
+    valorMinimo=""
+    valorMaximo=""
+
+    # Diccionario para mapear los colores a códigos hexadecimales
+    colores_hex = {
+        '0': {'nombre': 'Negro', 'hex': '#000000'},
+        '1': {'nombre': 'Cafe', 'hex': '#804000'},
+        '2': {'nombre': 'Rojo', 'hex': '#FF0000'},
+        '3': {'nombre': 'Naranja', 'hex': '#FFA500'},
+        '4': {'nombre': 'Amarillo', 'hex': '#FFFF00'},
+        '5': {'nombre': 'Verde', 'hex': '#008000'},
+        '6': {'nombre': 'Azul', 'hex': '#0000FF'},
+        '7': {'nombre': 'Violeta', 'hex': '#EE82EE'},
+        '8': {'nombre': 'Gris', 'hex': '#808080'},
+        '9': {'nombre': 'Blanco', 'hex': '#FFFFFF'},
+        'oro': {'nombre': 'Oro', 'hex': '#FFD700'},
+        'plata': {'nombre': 'Plata', 'hex': '#C0C0C0'}
+    }
+
+    colores_hexa = {
+        '1': {'nombre': 'Negro', 'hex': '#000000'},
+        '10': {'nombre': 'Cafe', 'hex': '#804000'},
+        '100': {'nombre': 'Rojo', 'hex': '#FF0000'},
+        '1000': {'nombre': 'Naranja', 'hex': '#FFA500'},
+        '10000': {'nombre': 'Amarillo', 'hex': '#FFFF00'},
+        '100000': {'nombre': 'Verde', 'hex': '#008000'},
+        '1000000': {'nombre': 'Azul', 'hex': '#0000FF'},
+        '10000000': {'nombre': 'Violeta', 'hex': '#EE82EE'},
+        '100000000': {'nombre': 'Gris', 'hex': '#808080'},
+        '1000000000': {'nombre': 'Blanco', 'hex': '#FFFFFF'}
+    }
+
+    registros=[]
+
+    resistencias_form=forms.calculaResistencias(request.form)
+    if request.method == 'POST':
+        color1=resistencias_form.color1.data
+        color2=resistencias_form.color2.data
+        color3=resistencias_form.color3.data
+        
+        valorColores = int(str(color1+color2))*int(color3)
+        tolerancia= request.form.get("tolerancia")
+        
+        if str(tolerancia) == "oro":
+            valorMinimo = valorColores - (valorColores*0.05)
+            valorMaximo = valorColores + (valorColores*0.05)
+        elif str(tolerancia) == "plata":
+            valorMinimo = valorColores - (valorColores*0.10)
+            valorMaximo = valorColores + (valorColores*0.10)
+
+        registros.append({
+            "color1": colores_hex[color1]['nombre'],
+            "color2": colores_hex[color2]['nombre'],
+            "color3": colores_hexa[color3]['nombre'],
+            "tolerancia":tolerancia,
+            "valor":valorColores,
+            "valorMinimo":valorMinimo,
+            "valorMaximo":valorMaximo,
+            "colorCelda1": colores_hex[color1]['hex'],
+            "colorCelda2": colores_hex[color2]['hex'],
+            "colorCelda3": colores_hexa[color3]['hex'],
+            "colorCelda4": colores_hex[tolerancia]['hex']
+        })
+        
+
+    return render_template("resistencias.html",form=resistencias_form,registros=registros)
 
 @app.route("/resultado",methods=["GET","POST"])
 def resultado():
